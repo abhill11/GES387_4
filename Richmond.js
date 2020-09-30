dd00990b28e74eec1da192eda7a8943fcb821898
abhill11 <70242250+abhill11@users.noreply.github.com>
2020-09-30 17:56:08 -0500

    Add point layer

---
+++ Richmond.js
@@ -0,0 +1,8 @@
+var Richmond = {
+"type": "FeatureCollection",
+"name": "Richmond",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0, "Lat": 37.5407, "Long": -77.436 }, "geometry": { "type": "Point", "coordinates": [ -77.435997328762284, 37.540711556456138 ] } }
+]
+}
